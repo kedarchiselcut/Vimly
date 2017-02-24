01cec63adb322c07fe1084ebd525bd1775bedf28
Kedar <kedar@chiselcut.in>
2017-02-24 11:06:31 +0530

    Added function to get videos by pagenumber and albumid

--- Vimly/ViewController.swift
+++ Vimly/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func getVideos(albumId: Int, pageNumber: Int) {
+        let parameters = ["page": pageNumber]
+        let urlString = String(format: "http://vimeo.com/api/v2/album/%d/videos.json?page=%d", albumId, pageNumber)
+        
+        Alamofire.request(urlString, parameters: parameters).validate().responseJSON { response in
+            switch response.result {
+            case .success:
+                let videosArray: Array<Any> = response.result.value as! Array<Any>
+                print(videosArray)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
